4a6e4ee08f4a943cc226b8f117514f7fb8affe93
oleg <gusevoleg@gmail.com>
2021-08-25 08:01:13 +0300

    test

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path
 
-from core.views import test_dava, test_car
+from core.views import test_dava, test_car, test
 urlpatterns = [
     path('test_dava', test_dava),
-    path('test_car', test_car)
+    path('test_car', test_car),
+    path('test', test),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,15 +1,21 @@
+import asyncio
 import datetime
 
 from django.shortcuts import render
-
+import datetime
+from django.utils import timezone
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
+from core.models import Proxy
 from core.parsing_by_hashtag import parsing_hashtag
 from core.parsing_by_username import parsing_username
+from core.scrapping.scrapers.hashtag import parsing_by_hashtag
+from core.utils.proxy import stop_proxy, get_proxies
+from core.utils.utils import update_time_timezone
 
 
 @csrf_exempt
@@ -30,4 +36,39 @@ def test_car(request):
     s = False
     while s == False:
         s = parsing_hashtag('s')
+    return Response("Ok")
+
+@csrf_exempt
+@api_view(["GET"])
+@permission_classes((AllowAny,))
+def test(request):
+    url = f"https://www.tiktok.com/tag/spb"
+    loop = asyncio.new_event_loop()
+    proxy = Proxy.objects.filter(banned=False, captcha=False,
+                                      last_used__lte=update_time_timezone(
+                                          timezone.localtime()
+                                      ) - datetime.timedelta(minutes=5)).order_by('taken', 'last_used').first()
+    print(1)
+
+    if proxy is not None:
+        proxy.taken = True
+        proxy.save(update_fields=['taken'])
+        proxies = get_proxies(proxy)
+    print(2)
+
+    if proxy is None:
+        return None
+    print(3)
+
+    try:
+        result = loop.run_until_complete(asyncio.wait_for(parsing_by_hashtag(url, proxies), 30000))
+    except Exception as e:
+        print(e)
+        stop_proxy(proxy, banned=1)
+        return False
+    stop_proxy(proxy, result.captcha)
+    print(4)
+
+    if not result.success:
+        return False
     return Response("Ok")
