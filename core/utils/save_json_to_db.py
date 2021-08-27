8d254c4c4dff8b668f502d0effbf843f49f3920f
oleg <gusevoleg@gmail.com>
2021-08-27 12:50:53 +0300

    fix

--- core/utils/save_json_to_db.py
+++ core/utils/save_json_to_db.py
@@ -14,73 +14,76 @@ def save(result_posts):
     post_hashtag = []
     authors = []
     authors_description = []
-    for post in result_posts:
-        try:
-            print("save post" + str(post['id']))
-            url = f"https://www.tiktok.com/@{post['author']['uniqueId']}/video/{post['id']}"
-            posts.append(Post(
-                id=post['id'],
-                user_id=post.get('author', {}).get('id'),
-                music_id=post.get('music', {}).get('id'),
-                created_date=datetime.datetime.fromtimestamp(post['createTime']),
-                url=url,
-                likes=post.get('stats', {}).get('diggCount'),
-                reposts=post.get('stats', {}).get('shareCount'),
-                viewed=post.get('stats', {}).get('playCount'),
-                sphinx_id=get_sphinx_id(url),
-                content_hash=get_md5_text(post.get('desc')),
-                comments=post.get('stats', {}).get('commentCount')
-            )
-            )
+    try:
+        for post in result_posts:
             try:
-                print("save append " + str(post['id']))
-                posts_content.append(PostContent(id=post['id'], description=post.get('desc')))
-            except Exception:
-                pass
-            try:
-                music.append(
-                    Music(id=post['music']['id'], author_nickname=post.get('music', {}).get('authorName'), title=post['music']['title'])
+                print("save post" + str(post['id']))
+                url = f"https://www.tiktok.com/@{post['author']['uniqueId']}/video/{post['id']}"
+                posts.append(Post(
+                    id=post['id'],
+                    user_id=post.get('author', {}).get('id'),
+                    music_id=post.get('music', {}).get('id'),
+                    created_date=datetime.datetime.fromtimestamp(post['createTime']),
+                    url=url,
+                    likes=post.get('stats', {}).get('diggCount'),
+                    reposts=post.get('stats', {}).get('shareCount'),
+                    viewed=post.get('stats', {}).get('playCount'),
+                    sphinx_id=get_sphinx_id(url),
+                    content_hash=get_md5_text(post.get('desc')),
+                    comments=post.get('stats', {}).get('commentCount')
                 )
-            except Exception:
-                pass
-            try:
-                createTime = post["author"].get("createTime", None)
-            except Exception:
-                createTime = None
-            if createTime is not None:
+                )
                 try:
-                    createTime = datetime.datetime.fromtimestamp(createTime)
+                    print("save append " + str(post['id']))
+                    posts_content.append(PostContent(id=post['id'], description=post.get('desc')))
                 except Exception:
                     pass
-            try:
-                authors.append(
-                    Author(
-                        id=post["author"]["id"],
-                        username=post.get("author", {}).get("uniqueId"),
-                        nickname=post.get("author", {}).get("nickname"),
-                        created_date=createTime,
-                        url=f"https://www.tiktok.com/@{post.get('author', {}).get('uniqueId')}",
-                        followers=post.get("authorStats", {}).get("followerCount"),
-                        following=post.get("authorStats", {}).get("followingCount"),
-                        likes=post.get("authorStats", {}).get("heartCount"),
-                        digg=post.get("authorStats", {}).get("diggCount")
+                try:
+                    music.append(
+                        Music(id=post['music']['id'], author_nickname=post.get('music', {}).get('authorName'), title=post['music']['title'])
                     )
-                )
-            except Exception:
-                pass
-            try:
-                authors_description.append(
-                    AuthorDescription(id=post["author"]["id"], description=post["author"]["signature"]))
-            except Exception:
-                pass
-            for hashtag in post.get("challenges", []):
-                try:
-                    hashtags.append(Hashtag(id=hashtag["id"], name=hashtag['title']))
-                    post_hashtag.append(PostHashtag(post_id=post['id'], hashtag_id=hashtag["id"]))
                 except Exception:
                     pass
-        except Exception as e:
-            print(e)
+                try:
+                    createTime = post["author"].get("createTime", None)
+                except Exception:
+                    createTime = None
+                if createTime is not None:
+                    try:
+                        createTime = datetime.datetime.fromtimestamp(createTime)
+                    except Exception:
+                        pass
+                try:
+                    authors.append(
+                        Author(
+                            id=post["author"]["id"],
+                            username=post.get("author", {}).get("uniqueId"),
+                            nickname=post.get("author", {}).get("nickname"),
+                            created_date=createTime,
+                            url=f"https://www.tiktok.com/@{post.get('author', {}).get('uniqueId')}",
+                            followers=post.get("authorStats", {}).get("followerCount"),
+                            following=post.get("authorStats", {}).get("followingCount"),
+                            likes=post.get("authorStats", {}).get("heartCount"),
+                            digg=post.get("authorStats", {}).get("diggCount")
+                        )
+                    )
+                except Exception:
+                    pass
+                try:
+                    authors_description.append(
+                        AuthorDescription(id=post["author"]["id"], description=post["author"]["signature"]))
+                except Exception:
+                    pass
+                for hashtag in post.get("challenges", []):
+                    try:
+                        hashtags.append(Hashtag(id=hashtag["id"], name=hashtag['title']))
+                        post_hashtag.append(PostHashtag(post_id=post['id'], hashtag_id=hashtag["id"]))
+                    except Exception:
+                        pass
+            except Exception as e:
+                print(e)
+    except Exception as e:
+        print(e)
     print(" Post.objects.bulk_create(posts, batch_size=batch_size, ignore_conflicts=True)")
     try:
         Post.objects.bulk_create(posts, batch_size=batch_size, ignore_conflicts=True)
