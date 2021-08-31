71637843299d15170b4583448985185048e75140
oleg <gusevoleg@gmail.com>
2021-08-31 11:44:58 +0300

    fix

--- core/scrapping/scraper.py
+++ core/scrapping/scraper.py
@@ -28,6 +28,7 @@ class BrowserManager:
         # set_chromium_version(self.chromium_version)
 
         headless = os.getenv('HEADLESS', 'true').lower() == 'true'
+        print('-proxy-server')
         print(  f'--proxy-server={self.proxy.ip}:{self.proxy.port}')
         self.browser = await launch(headless=headless,
                                     handleSIGINT=False,
