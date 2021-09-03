2e1212e72fd7a6637a5b9862f53c57dd1442e8c5
oleg <gusevoleg@gmail.com>
2021-09-03 10:23:10 +0300

    parsing name in cooratninus

--- core/parsing_by_username.py
+++ core/parsing_by_username.py
@@ -3,26 +3,40 @@ import asyncio
 from core.scrapping.scrapers.account import parsing_account
 from core.utils.proxy import stop_proxy, get_proxy
 from core.utils.save_json_to_db import save
+from core.utils.utils import update_time_timezone
+from django.utils import timezone
 
 
-def parsing_username(username, parsing_to=None):
-    # TODO retro
-    url = f"https://www.tiktok.com/@{username}"
+def parsing_username(sources_item, parsing_to=None):
+    url = f"https://www.tiktok.com/@{sources_item.data}"
     loop = asyncio.new_event_loop()
     proxy, proxy_data = get_proxy()
     if proxy is None:
+        sources_item.taken = 0
+        sources_item.save(update_fields=["taken"])
         return None
     print("start")
+    is_success = True
+    result = None
     try:
         result = loop.run_until_complete(asyncio.wait_for(parsing_account(url, proxy_data, parsing_to), 300_000))
     except Exception as e:
         print(e)
+        is_success = False
         stop_proxy(proxy, banned=1)
-        return False
-
-    stop_proxy(proxy, result.captcha)
-    save(result.body)
-
     if not result.success:
-        return False
-    return True
+        is_success = False
+    stop_proxy(proxy, result.captcha)
+    try:
+        save(result.body)
+    except Exception:
+        is_success = False
+
+    sources_item.taken = 0
+    if is_success:
+        sources_item.last_modified = update_time_timezone(timezone.localtime())
+        sources_item.save(update_fields=["taken", "last_modified"])
+    else:
+        sources_item.save(update_fields=["taken"])
+    return result
+
--- tiktok/tasks.py
+++ tiktok/tasks.py
@@ -15,7 +15,7 @@ from django.utils import timezone
 logger = logging.getLogger(__file__)
 
 MAX_SIZE_PARSE_BY_WORD = 5
-
+MAX_SIZE_PARSE_IN_CHANNEL = 4
 
 @app.task
 def start_task_parsing_hashtags():
@@ -67,43 +67,56 @@ def start_task_parsing_accounts():
         Q(retro_max__isnull=True) | Q(retro_max__gte=timezone.now()), published=1,
         status=1)
     # network_id
-    sources_items = SourcesItems.objects.filter(network_id=9, disabled=0, taken=0,
+    all_sources_items = SourcesItems.objects.filter(network_id=9, disabled=0, taken=0,
                                                 source_id__in=list(select_sources.values_list('id', flat=True)))
-    null_sources_items = sources_items.filter(last_modified__isnull=True)
+    null_sources_items = all_sources_items.filter(last_modified__isnull=True)
     if len(null_sources_items) > 0:
-        sources_item = null_sources_items.first()
+        sources_items = null_sources_items
     else:
-        sources_item = sources_items.order_by('last_modified').first()
-    print(sources_item)
-    if sources_item:
-        time = select_sources.get(id=sources_item.source_id).sources
-        if time is None:
-            time = 0
-        if sources_item.last_modified is None or (
-                sources_item.last_modified + datetime.timedelta(minutes=time) <
-                update_time_timezone(timezone.localtime())):
+        sources_items = all_sources_items
 
-            retro_date = select_sources.get(id=sources_item.source_id).retro
-            last_update = sources_item.last_modified
-            parsing_to = retro_date
-            try:
-                if not sources_item.foced and last_update is not None:
-                    parsing_to = last_update.date()
-            except Exception:
-                pass
-            result = False
-            try:
-                sources_item.taken = 1
-                sources_item.save()
-                attempt = 0
-                while not result and attempt < 10:
-                    result = parsing_username(sources_item.data, parsing_to)
+    pool_source = ThreadPoolExecutor(3)
+    iteration = 0
+    futures = []
 
-                    attempt += 1
-            except Exception as e:
-                print(e)
-            if result:
-                sources_item.last_modified = update_time_timezone(timezone.localtime())
-                sources_item.save(update_fields=["taken", "last_modified"])
-            else:
-                sources_item.save(update_fields=["taken"])
+    for sources_item in sources_items:
+        try:
+            if iteration > MAX_SIZE_PARSE_IN_CHANNEL or \
+                    SourcesItems.objects.filter(network_id=8, disabled=0, taken=1,
+                                                source_id__in=list(select_sources.values_list('id', flat=True))
+                                                ).count() > MAX_SIZE_PARSE_IN_CHANNEL:
+                break
+
+            time = select_sources.get(id=sources_item.source_id).sources
+            if time is None:
+                time = 0
+            if sources_item.last_modified is None or (
+                    sources_item.last_modified + datetime.timedelta(minutes=time) <
+                    update_time_timezone(timezone.localtime())):
+
+                retro_date = select_sources.get(id=sources_item.source_id).retro
+                last_update = sources_item.last_modified
+                parsing_to = retro_date
+                try:
+                    if not sources_item.foced and last_update is not None:
+                        parsing_to = last_update.date()
+                except Exception:
+                    pass
+                result = False
+                try:
+                    sources_item.taken = 1
+                    sources_item.save(update_fields=["taken"])
+                    futures.append(
+                        pool_source.submit(
+                            parsing_username, sources_item, parsing_to
+                        )
+                    )
+
+                except Exception as e:
+                    print(e)
+        except Exception:
+            pass
+        iteration += 1
+        # TODO taken=0
+    for future in concurrent.futures.as_completed(futures, timeout=1000):
+        print(future.result())
