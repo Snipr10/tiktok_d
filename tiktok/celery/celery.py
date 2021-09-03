681dea442171ed6ee7d57de1096098ab7410bc1b
oleg <gusevoleg@gmail.com>
2021-09-03 10:23:55 +0300

    test

--- tiktok/celery/celery.py
+++ tiktok/celery/celery.py
@@ -14,13 +14,13 @@ app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
 
-    'start_task_parsing_hashtags': {
-        'task': 'tiktok.tasks.start_task_parsing_hashtags',
-        'schedule': crontab(minute='*/5')
-    },
-
-    # 'start_task_parsing_accounts': {
-    #     'task': 'tiktok.tasks.start_task_parsing_accounts',
-    #     'schedule': crontab(minute='*/10')
+    # 'start_task_parsing_hashtags': {
+    #     'task': 'tiktok.tasks.start_task_parsing_hashtags',
+    #     'schedule': crontab(minute='*/5')
     # },
+
+    'start_task_parsing_accounts': {
+        'task': 'tiktok.tasks.start_task_parsing_accounts',
+        'schedule': crontab(minute='*/1')
+    },
 }
