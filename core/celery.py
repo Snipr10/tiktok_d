a597cdd07ae670853fc065a894e90672348e8487
oleg <gusevoleg@gmail.com>
2021-09-08 19:15:48 +0300

    test

--- core/celery.py
+++ core/celery.py
@@ -13,26 +13,26 @@ app.config_from_object('django.conf:settings')
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
-    #
-    # 'start_task_parsing_hashtags': {
-    #     'task': 'tiktok.tasks.start_task_parsing_hashtags',
-    #     'schedule': crontab(minute='*/3')
-    # },
-    #
-    # 'start_task_parsing_accounts': {
-    #     'task': 'tiktok.tasks.start_task_parsing_accounts',
-    #     'schedule': crontab(minute='*/5')
-    # },
-    # 'start_task_webhook': {
-    #     'task': 'tiktok.tasks.start_task_webhook',
-    #     'schedule': crontab(minute='*/50')
-    # },
+
+    'start_task_parsing_hashtags': {
+        'task': 'tiktok.tasks.start_task_parsing_hashtags',
+        'schedule': crontab(minute='*/3')
+    },
+
+    'start_task_parsing_accounts': {
+        'task': 'tiktok.tasks.start_task_parsing_accounts',
+        'schedule': crontab(minute='*/2')
+    },
+    'start_task_webhook': {
+        'task': 'tiktok.tasks.start_task_webhook',
+        'schedule': crontab(minute='*/50')
+    },
     'stop_keys': {
         'task': 'tiktok.tasks.stop_keys',
-        'schedule': crontab(minute='*/1')
+        'schedule': crontab(minute='*/5')
     },
     'stop_channels': {
         'task': 'tiktok.tasks.stop_channels',
-        'schedule': crontab(minute='*/1')
+        'schedule': crontab(minute='*/5')
     },
 }
